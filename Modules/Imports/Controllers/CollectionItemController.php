1d91a1cc3d606876cceb404879104b0de51407e7
kostiantynzvir <zvirkost@outlook.com>
2023-12-15 11:50:09 -0500

    delete feature

--- Modules/Imports/Controllers/CollectionItemController.php
+++ Modules/Imports/Controllers/CollectionItemController.php
@@ -5,21 +5,19 @@ namespace Modules\Imports\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Modules\Imports\Contracts\UpdatesCollectionItem;
-use Modules\Imports\Contracts\UpdatesImageInCell;
 use Modules\Imports\Models\CollectionItem;
 
 class CollectionItemController extends Controller
 {
-    public function destroy(Request $request, CollectionItem $collectionItem)
+    public function destroy(CollectionItem $collectionItem)
     {
-        error_log('delete');
-
+        $collectionItem->delete();
         return back(303);
     }
 
     public function update(Request $request, CollectionItem $collectionItem)
     {
-        $updater = app(UpdatesImageInCell::class);
+        $updater = app(UpdatesCollectionItem::class);
 
         $updater->update($request->user(), $collectionItem, $request->all());
 
