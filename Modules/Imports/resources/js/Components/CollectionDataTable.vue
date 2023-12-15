403cca027b5195ac3be631613cb13fe8c424bd56
rkablaoui <serhiimartyniuk1@outlook.com>
2023-12-15 04:19:11 -0500

    update file parameter: source, title, size

--- Modules/Imports/resources/js/Components/CollectionDataTable.vue
+++ Modules/Imports/resources/js/Components/CollectionDataTable.vue
@@ -22,14 +22,14 @@ const colNumber = (index) => {
     <div>
         <Pagination :links="items.links" />
         <ul role="list" class="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
-            <li v-for="file in items.data" :key="file.uploaded_file_path" class="relative">
+            <li v-for="file in items.data" :key="file.source" class="relative">
                 <div class="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
-                    <img :src="file.uploaded_file_path" alt="" class="pointer-events-none object-cover group-hover:opacity-75" />
+                    <img :src="file.source" alt="" class="pointer-events-none object-cover group-hover:opacity-75" />
                     <button type="button" class="absolute inset-0 focus:outline-none">
-                    <span class="sr-only">View details for {{ file.original_file_name }}</span>
+                    <span class="sr-only">View details for {{ file.title }}</span>
                     </button>
                 </div>
-            <p class="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">{{ file.original_file_name }}</p>
+            <p class="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">{{ file.title }}</p>
             <p class="pointer-events-none block text-sm font-medium text-gray-500">{{ file.size }}</p>
             </li>
         </ul>
